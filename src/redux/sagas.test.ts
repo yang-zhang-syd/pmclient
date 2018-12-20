3b4d02e7fe873fc02dd6570a9721ab6d6c137328
Yang Zhang <yang.zhang@icloud.com>
2018-12-20 20:22:33 +1100

    Test Stock Created Action is created after response is received.

--- src/redux/sagas.test.ts
+++ src/redux/sagas.test.ts
@@ -1,8 +1,12 @@
 import { createStock } from './sagas';
 import axios from 'axios';
-import { call } from 'redux-saga/effects'
+import { call, put } from 'redux-saga/effects'
+import { actions } from './actions';
+
+const dummyStockCreatedData = {data: {dummyData: 'dummyData'}};
 
 test('createStock saga test', () => {
     const gen = createStock({symbol: 'TestSymbol'});
     expect(gen.next().value).toEqual(call(axios.post, `http://localhost/api/stock`, {"Symbol": "TestSymbol"}));
+    expect(gen.next(dummyStockCreatedData).value).toEqual(put(actions.stockCreated(dummyStockCreatedData.data)));
 });
