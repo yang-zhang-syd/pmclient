01e32d44bad9c63a1ac2185172e005d5169a2086
Yang Zhang <yang.zhang@icloud.com>
2019-01-02 22:29:11 +1100

    Added getStocks saga test.

--- src/redux/sagas.test.ts
+++ src/redux/sagas.test.ts
@@ -1,12 +1,19 @@
-import { createStock } from './sagas';
+import { createStock, getStocks } from './sagas';
 import axios from 'axios';
 import { call, put } from 'redux-saga/effects'
 import { actions } from './actions';
 
 const dummyStockCreatedData = {data: {dummyData: 'dummyData'}};
+const getStcoksResponse : any = {data: []};
 
 test('createStock saga test', () => {
     const gen = createStock({symbol: 'TestSymbol'});
     expect(gen.next().value).toEqual(call(axios.post, `http://localhost/api/stock`, {"Symbol": "TestSymbol"}));
     expect(gen.next(dummyStockCreatedData).value).toEqual(put(actions.stockCreated(dummyStockCreatedData.data)));
+});
+
+test('getStocks saga test', () => {
+    const gen = getStocks({pageNum: 1, pageSize: 10});
+    expect(gen.next().value).toEqual(call(axios.post, `http://localhost/api/stock/stocks/page/1`, {pageSize: 10}));
+    expect(gen.next(getStcoksResponse).value).toEqual(put(actions.stocksReceived(getStcoksResponse.data)));
 });
